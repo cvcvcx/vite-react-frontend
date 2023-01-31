fa72a42460c00db380badb3e9ed32d95db2d24f4
cvcvcx9 <cvcvcx9@gmaill.com>
2023-01-31 15:41:43 +0900

    [update] 검색기능 구현

--- src/pages/GuestBookList.jsx
+++ src/pages/GuestBookList.jsx
@@ -4,13 +4,11 @@ import Paper from "@mui/material/Paper";
 
 import ListRegisterBtn from "../components/list/ListRegisterBtn";
 import Orders from "../components/list/Orders";
-import Search from "../components/list/Search";
 
 const GuestBookList = () => {
   return (
     <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
       <Orders />
-      <Search />
       <ListRegisterBtn />
     </Paper>
   );
